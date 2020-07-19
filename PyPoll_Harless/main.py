ec10e8f6b50f04103cffb89a669e647cb2c86b5f
RHARLESS <rharless99@gmail.com>
2020-07-19 14:13:50 -0700

    import os and csv

---
+++ PyPoll_Harless/main.py
@@ -0,0 +1,22 @@
+import os
+import csv
+
+
+electionpath = os.path.join('Resources', 'election_data.csv')
+
+# Method 2: Improved Reading using CSV module
+
+with open(electionpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first 
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)

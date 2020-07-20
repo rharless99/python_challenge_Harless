0731bb3d298cd26baf2cb7bb551c145d6b9d989d
RHARLESS <rharless99@gmail.com>
2020-07-19 19:31:09 -0700

    For loop for total months and profit loss totals

--- PyBank_Harless/main.py
+++ PyBank_Harless/main.py
@@ -15,8 +15,17 @@ with open(budgetpath) as csvfile:
 
     # Read the header row first 
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-
+    #print(f"CSV Header: {csv_header}")
+    print("FINANCIAL ANALYSIS")
+    print("_" * 50)
     # Read each row of data after the header
+    month_count = 0
+    Total_profit_loss = 0
+    # For loop tofind total number of months and total amount fo profit/losses
     for row in csvreader:
-        print(row)
+        month_count = month_count +1
+        Total_profit_loss = int(row[1]) + Total_profit_loss
+    print(f"Total Months:  {month_count}")
+    print(f"Total: {Total_profit_loss}")
+    average_change = Total_profit_loss/month_count
+    print(f"Average Change: {average_change}")
